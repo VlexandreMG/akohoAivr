c4c2eaeddd48f6a0e57195132e040bd87de9491b
VlexandreMG <batmanhoodie23mahery@gmail.com>
2026-03-16 20:15:26 +0300

    Changement de la classe race (seulement la classe)

--- backend/models/Race.js
+++ backend/models/Race.js
@@ -1,7 +1,43 @@
 class Race {
-    constructor(id, nom) {
+    constructor(id, nom, percentMale, percentFemale,capPondaison,percentLamokana) {
         this.id = id;
         this.nom = nom;
+        this.percentMale = percentMale;
+        this.percentFemale = percentFemale;
+        this.capPondaison = capPondaison;
+        this.percentLamokana = percentLamokana;
+    }
+
+    getPercentMale() {
+        return this.percentMale;
+    }
+
+    getPercentFemale() {
+        return this.percentFemale;
+    }
+    
+    getCapPondaison() {
+        return this.capPondaison;
+    }
+
+    getPercentLamokana() {
+        return this.percentLamokana;
+    }
+
+    setPercentMale(percentMale) {
+        this.percentMale = percentMale;
+    }
+
+    setPercentFemale(percentFemale) {
+        this.percentFemale = percentFemale;
+    }
+    
+    setCapPondaison(capPondaison) {
+        this.capPondaison = capPondaison;
+    }
+
+    setPercentLamokana(percentLamokana) {
+        this.percentLamokana = percentLamokana;
     }
 
     getId() {
